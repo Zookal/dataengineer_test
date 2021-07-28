f0f41d9307085c11a344c94593cc4b14cd157a7e
Mark <us66@klavenessdigital.com>
2021-07-28 19:53:49 +0800

    Folder blueprint for development

---
+++ retail_etl/dags/retail_dag.py
@@ -0,0 +1,24 @@
+import airflow.utils.dates
+from airflow.models import DAG
+from airflow.operators.dummy import DummyOperator
+
+
+dag = DAG(
+    dag_id="retail_dag",
+    schedule_interval="*/15 * * * *",
+    start_date=airflow.utils.dates.days_ago(1),
+    catchup=False
+)
+
+begin_execution = DummyOperator(
+    task_id="begin_execution",
+    dag=dag
+)
+
+end_execution = DummyOperator(
+    task_id="end_execution",
+    dag=dag
+)
+
+
+begin_execution >> end_execution
