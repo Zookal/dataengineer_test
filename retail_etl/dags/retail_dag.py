9cbaa96e1fc398e35d827dc7167e8cbfb7c48cf0
Mark <us66@klavenessdigital.com>
2021-07-29 11:39:37 +0800

    Retail dag auto formatted by black

--- retail_etl/dags/retail_dag.py
+++ retail_etl/dags/retail_dag.py
@@ -7,18 +7,14 @@ dag = DAG(
     dag_id="retail_dag",
     schedule_interval="*/15 * * * *",
     start_date=airflow.utils.dates.days_ago(1),
-    catchup=False
+    catchup=False,
 )
 
-begin_execution = DummyOperator(
-    task_id="begin_execution",
-    dag=dag
-)
 
-end_execution = DummyOperator(
-    task_id="end_execution",
-    dag=dag
-)
+begin_execution = DummyOperator(task_id="begin_execution", dag=dag)
+
+
+end_execution = DummyOperator(task_id="end_execution", dag=dag)
 
 
 begin_execution >> end_execution
