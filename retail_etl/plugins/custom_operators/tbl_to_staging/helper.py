a498131f9c1f67f57811249dc1a8c05761226369
Mark <us66@klavenessdigital.com>
2021-07-31 12:41:39 +0800

    Modularization of TblToStageOperator class
    Separation of reading of data from, and loading of data to MySQL db
    by creating functions for each of them.

--- retail_etl/plugins/custom_operators/tbl_to_staging/helper.py
+++ retail_etl/plugins/custom_operators/tbl_to_staging/helper.py
@@ -1,6 +1,10 @@
-from typing import Dict, Union
+import traceback
+from typing import Dict, Union, Optional
+import logging
 
 import pandas as pd
+from airflow.models import Connection
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from pandas.io.parsers import TextFileReader
 
 from custom_operators.tbl_to_staging import model
@@ -18,6 +22,11 @@ _HEADER_MAPPING = {
 }
 
 
+def _get_logger(logger_name: Optional[str]) -> logging.Logger:
+    logger = logging.getLogger(logger_name if logger_name else "airflow.task")
+    return logger
+
+
 def _get_header(
     *, table_name: str
 ) -> Union[
@@ -31,7 +40,14 @@ def _get_header(
     model.LineItemHeader,
     None,
 ]:
+    """Map a table name into it's header counterpart."""
     return _HEADER_MAPPING.get(table_name)
+
+
+def _create_mysql_connection(mysql_conn_id: Optional[str]) -> Connection:
+    mysql_hook = MySqlHook(mysql_conn_id=mysql_conn_id if mysql_conn_id else "mysql_default")
+    mysql_conn = mysql_hook.get_conn()
+    return mysql_conn
 
 
 def get_dataframe(table_name: str, **pandas_read_args: Dict) -> TextFileReader:
@@ -39,3 +55,34 @@ def get_dataframe(table_name: str, **pandas_read_args: Dict) -> TextFileReader:
     header = _get_header(table_name=table_name)
     columns = header.to_list() if header else None
     return pd.read_csv(**pandas_read_args, names=columns, usecols=columns)
+
+
+def load_to_mysql_db(df_batches: TextFileReader, **data_load_args: Dict) -> int:
+    """
+    Loads batches of data to MySQL Database.
+
+    :param df_batches: An iterator like object
+        which contains the batches of DataFrames to be loaded.
+    :param data_load_args: The arguments needed to connect
+        and perform insert query into the Database.
+
+    :return: Total number of rows successfully loaded.
+    """
+    logger = _get_logger(logger_name=data_load_args.get("logger_name"))
+    mysql_conn = _create_mysql_connection(mysql_conn_id=data_load_args.get("mysql_conn_id"))
+    mysql_cursor = mysql_conn.cursor()
+    total_inserted_rows = 0
+    for _df in df_batches:  # type: pd.DataFrame
+        nrows = len(_df)
+        logger.debug(f"Inserting {nrows} rows in the {data_load_args.get('table_name')} table...")
+        try:
+            rows = (tuple(row) for row in _df.itertuples(index=False))
+            mysql_cursor.executemany(data_load_args.get("upsert_query"), rows)
+            total_inserted_rows += nrows
+        except Exception:
+            mysql_conn.rollback()
+            logger.debug(f"The TblToStageOperator process has failed {traceback.format_exc()}")
+        finally:
+            mysql_conn.commit()
+    mysql_conn.close()
+    return total_inserted_rows
--- retail_etl/plugins/custom_operators/tbl_to_staging/tbl_to_staging.py
+++ retail_etl/plugins/custom_operators/tbl_to_staging/tbl_to_staging.py
@@ -1,54 +1,31 @@
 from typing import Dict
-import traceback
 
-import pandas as pd
 from airflow.models import BaseOperator
-from airflow.providers.mysql.hooks.mysql import MySqlHook
 
 from custom_operators.tbl_to_staging import helper
 
 
 class TblToStageOperator(BaseOperator):
     def __init__(
-        self, mysql_conn_id: str, table_name: str, pandas_read_args: Dict, upsert_query: str, *args, **kwargs
+        self, pandas_read_args: Dict, data_load_args: Dict, *args, **kwargs
     ):
         """
         Operator to import a .tbl file to a MySQL database.
 
-        :param mysql_conn_id: The registered connection ID of MySQL connection.
         :param pandas_read_args: The arguments that will be used for reading the .tbl files
-        :param upsert_query: The upsert query for the corresponding given table.
+        :param data_load_args: A key/value pair with the args that will be used for loading a batch of data.
         :param args: Any additional args that BaseOperator can use.
         :param kwargs: Any additional keyword args that BaseOperator can use.
         """
         super().__init__(*args, **kwargs)
-        self._mysql_conn_id = mysql_conn_id
+        self._data_load_args = data_load_args
         self._pandas_read_args = pandas_read_args
-        self._table_name = table_name
-        self._upsert_query = upsert_query
 
     def execute(self, context: Dict):
         self.log.info("TblToStageOperator Starting...")
-        mysql_hook = MySqlHook(mysql_conn_id=self._mysql_conn_id)
-        mysql_conn = mysql_hook.get_conn()
-        mysql_cursor = mysql_conn.cursor()
 
-        df_batches = helper.get_dataframe(**self._pandas_read_args, table_name=self._table_name)
-        total_inserted_rows = 0
+        df_batches = helper.get_dataframe(**self._pandas_read_args)
+        total_inserted_rows = helper.load_to_mysql_db(**self._data_load_args, df_batches=df_batches)
+        table_name = self._data_load_args.get("table_name")
 
-        for _df in df_batches:  # type: pd.DataFrame
-            nrows = len(_df)
-            self.log.info(f"Inserting {nrows} rows in the {self._table_name} table...")
-
-            try:
-                rows = (tuple(row) for row in _df.itertuples(index=False))
-                mysql_cursor.executemany(self._upsert_query, rows)
-                total_inserted_rows += nrows
-            except Exception:
-                mysql_conn.rollback()
-                self.log.info(f"The TblToStageOperator process has failed {traceback.format_exc()}")
-            finally:
-                mysql_conn.commit()
-
-        mysql_conn.close()
-        self.log.info(f"Finished Inserting {total_inserted_rows} rows in the {self._table_name} table.")
+        self.log.info(f"Finished Loading {total_inserted_rows} rows in the {table_name} table.")
