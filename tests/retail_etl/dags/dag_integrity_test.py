9ae3bc2b26af006ad58fd581ea2e2d29d9f4f496
Mark <us66@klavenessdigital.com>
2021-07-31 17:22:13 +0800

    Removed unused variables in dag integrity test suite

--- tests/retail_etl/dags/dag_integrity_test.py
+++ tests/retail_etl/dags/dag_integrity_test.py
@@ -1,39 +1,15 @@
-import os
 import sys
-from collections import namedtuple
+
 from pathlib import Path
 
 import pytest
 from airflow.models import DAG
 from airflow.utils.dag_cycle_tester import test_cycle as _test_cycle
-from pytest_docker_tools import fetch, container
 
 from tests.retail_etl.dags.helper import dag_files, import_dag_file
 
 # NOTE: This is required to replicate how airflow manage its modules under the plugins folder.
 sys.path.append(f"{Path(__file__).parent.parent.parent.parent}/retail_etl/plugins")
-
-
-@pytest.fixture(scope="module")
-def postgres_credentials():
-    PostgresCredentials = namedtuple("PostgresCredentials", ["username", "password"])
-    return PostgresCredentials("testuser", "testpass")
-
-
-postgres_image = fetch(repository="postgres:13-alpine")
-postgres = container(
-    image="{postgres_image.id}",
-    environment={
-        "POSTGRES_USER": "{postgres_credentials.username}",
-        "POSTGRES_PASSWORD": "{postgres_credentials.password}",
-    },
-    ports={"5432/tcp": None},
-    volumes={
-        os.path.join(os.path.dirname(__file__), "postgres-init.sql"): {
-            "bind": "/docker-entrypoint-initdb.d/postgres-init.sql"
-        }
-    },
-)
 
 
 @pytest.mark.parametrize("dag_file", dag_files)
