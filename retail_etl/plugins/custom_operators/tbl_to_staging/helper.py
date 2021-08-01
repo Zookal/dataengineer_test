44ea6a2bc524ea1302b2776d718ed1d11f1b7093
Mark <us66@klavenessdigital.com>
2021-08-01 14:36:38 +0800

    Added execution time for query table filtering
    This is for counting of successful upserted data,
    such data can be obtained by getting all data with
    updated_at >= current timestamp.

--- retail_etl/plugins/custom_operators/tbl_to_staging/helper.py
+++ retail_etl/plugins/custom_operators/tbl_to_staging/helper.py
@@ -1,8 +1,10 @@
 import traceback
+from datetime import timedelta
 from typing import Dict, Union, Optional
 import logging
 
 import pandas as pd
+import pendulum
 from airflow.models import Connection
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 from pandas.io.parsers import TextFileReader
@@ -16,7 +18,7 @@ _HEADER_MAPPING = {
     "part": model.PartHeader(),
     "customer": model.CustomerHeader(),
     "supplier": model.SupplierHeader(),
-    "order": model.OrderHeader(),
+    "orders": model.OrderHeader(),
     "partsupp": model.PartSuppHeader(),
     "lineitem": model.LineItemHeader(),
 }
@@ -57,7 +59,7 @@ def get_dataframe(table_name: str, **pandas_read_args: Dict) -> TextFileReader:
     return pd.read_csv(**pandas_read_args, names=columns, usecols=columns)
 
 
-def load_to_mysql_db(df_batches: TextFileReader, **data_load_args: Dict) -> int:
+def load_to_mysql_db(df_batches: TextFileReader, execution_ts: pendulum.DateTime, **data_load_args: Dict) -> int:
     """
     Loads batches of data to MySQL Database.
 
@@ -65,24 +67,29 @@ def load_to_mysql_db(df_batches: TextFileReader, **data_load_args: Dict) -> int:
         which contains the batches of DataFrames to be loaded.
     :param data_load_args: The arguments needed to connect
         and perform insert query into the Database.
+    :param: execution_ts: The timestamp when this function was called.
 
     :return: Total number of rows successfully loaded.
     """
     logger = _get_logger(logger_name=data_load_args.get("logger_name"))
     mysql_conn = _create_mysql_connection(mysql_conn_id=data_load_args.get("mysql_conn_id"))
     mysql_cursor = mysql_conn.cursor()
+    table_name = data_load_args.get('table_name')
     total_inserted_rows = 0
-    for _df in df_batches:  # type: pd.DataFrame
+    for idx, _df in enumerate(df_batches):  # type: int, pd.DataFrame
         nrows = len(_df)
-        logger.debug(f"Inserting {nrows} rows in the {data_load_args.get('table_name')} table...")
+        logger.debug(f"Inserting {nrows} rows in the {table_name} table...")
         try:
             rows = (tuple(row) for row in _df.itertuples(index=False))
             mysql_cursor.executemany(data_load_args.get("upsert_query"), rows)
-            total_inserted_rows += nrows
         except Exception:
             mysql_conn.rollback()
             logger.error("The TblToStageOperator process has failed", exc_info=traceback.format_exc())
         finally:
             mysql_conn.commit()
+            mysql_cursor.execute(
+                f"SELECT COUNT(IF(updated_at >= '{execution_ts}', 1, NULL)) FROM {table_name};"
+            )
+            total_inserted_rows = mysql_cursor.fetchone()[0]
     mysql_conn.close()
     return total_inserted_rows
